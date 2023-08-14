242272e1e33a582a352e2172b23c6687fe43a8a7
Fabian Ruettgers <fabulon78@gmail.com>
2023-08-14 15:04:15 +0200

    bugfix 2

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <SWRConfig value={{ fetcher, refreshInterval: 1000 }}>
+      <SWRConfig value={{ fetcher }}>
         <GlobalStyle />
         <Component {...pageProps} />
       </SWRConfig>
