614542a1c2408bf7f2f90bffe40df66edeee2bf1
Fabian Ruettgers <fabulon78@gmail.com>
2023-08-14 11:21:36 +0200

    Render of the Data

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     styledComponents: true,
   },
   env: { API_KEY: "4c606aec01f0a1c7daed75110995b085" },
+  images: {
+    domains: ["image.tmdb.org"],
+  },
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import useSWR from "swr";
 
 const API_KEY = process.env.API_KEY;
@@ -6,11 +7,35 @@ export default function HomePage() {
   const { data, error } = useSWR(
     `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=de&sort_by=popularity.desc&api_key=${API_KEY}`
   );
+  if (!data) {
+    return <p>Loading ...</p>;
+  }
 
-  console.log(data);
+  if (!data.results || data.results.length === 0) {
+    return <p>Error</p>;
+  }
+
+  const randomMovie =
+    data.results[Math.floor(Math.random() * data.results.length)];
+
+  console.log(randomMovie);
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <main>
+      <div>
+        <div>
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${randomMovie.poster_path}`}
+            alt="Movie Poster"
+            width={400}
+            height={400}
+          />
+
+          <p>{randomMovie.vote_average} / 10</p>
+          <div>
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
