0844a8ad013401c6384b4a320d14d9f9f6a58b0f
Fabian Ruettgers <fabulon78@gmail.com>
2023-08-16 15:52:59 +0200

    local storrage stae

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   const [query, setQuery] = useState("");
-  const [bookmarkedMovies, setBookmarkedMovies] = useState({});
+  const [bookmarkedMovies, setBookmarkedMovies] = useLocalStorageState(
+    "Bookmarked",
+    { defaultValue: {} }
+  );
+
   return (
     <>
       <GlobalStyle />
