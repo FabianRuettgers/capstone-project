9c2df98771944b3f01c481001243ad855bf5064f
Fabian Ruettgers <fabulon78@gmail.com>
2023-08-18 11:43:11 +0200

    nav bugfix

--- components/Navigation/Footer/FooterNav/index.js
+++ components/Navigation/Footer/FooterNav/index.js
@@ -8,32 +8,31 @@ export default function FooterNav() {
   return (
     <StyledFooter>
       <MobileViewWrapper>
-        <Link href={"/"}>
-          <StyledLink isActive={router.pathname === "/"}>
-            <StyledSvg
-              isActive={router.pathname === "/"}
-              xmlns="http://www.w3.org/2000/svg"
-              height="64"
-              viewBox="0 -960 960 960"
-              width="64"
-            >
-              <path d="m140-800 74 152h130l-74-152h89l74 152h130l-74-152h89l74 152h130l-74-152h112q24 0 42 18t18 42v520q0 24-18 42t-42 18H140q-24 0-42-18t-18-42v-520q0-24 18-42t42-18Zm0 212v368h680v-368H140Zm0 0v368-368Z" />
-            </StyledSvg>
-          </StyledLink>
+        <Link href={"/"} isActivated={router.pathname === "/"}>
+          <StyledSvg
+            isActivated={router.pathname === "/"}
+            xmlns="http://www.w3.org/2000/svg"
+            height="64"
+            viewBox="0 -960 960 960"
+            width="64"
+          >
+            <path d="m123-240-43-43 292-291 167 167 241-241H653v-60h227v227h-59v-123L538-321 371-488 123-240Z" />
+          </StyledSvg>
         </Link>
 
-        <Link href={"/user-list"}>
-          <StyledLink isActive={router.pathname === "/user-list"}>
-            <StyledSvg
-              isActive={router.pathname === "/user-list"}
-              xmlns="http://www.w3.org/2000/svg"
-              height="64"
-              viewBox="0 -960 960 960"
-              width="64"
-            >
-              <path d="m140-800 74 152h130l-74-152h89l74 152h130l-74-152h89l74 152h130l-74-152h112q24 0 42 18t18 42v520q0 24-18 42t-42 18H140q-24 0-42-18t-18-42v-520q0-24 18-42t42-18Zm0 212v368h680v-368H140Zm0 0v368-368Z" />
-            </StyledSvg>
-          </StyledLink>
+        <Link
+          href={"/user-list"}
+          isActivated={router.pathname === "/user-list"}
+        >
+          <StyledSvg
+            isActivated={router.pathname === "/user-list"}
+            xmlns="http://www.w3.org/2000/svg"
+            height="64"
+            viewBox="0 -960 960 960"
+            width="64"
+          >
+            <path d="m140-800 74 152h130l-74-152h89l74 152h130l-74-152h89l74 152h130l-74-152h112q24 0 42 18t18 42v520q0 24-18 42t-42 18H140q-24 0-42-18t-18-42v-520q0-24 18-42t42-18Zm0 212v368h680v-368H140Zm0 0v368-368Z" />
+          </StyledSvg>
         </Link>
       </MobileViewWrapper>
     </StyledFooter>
@@ -62,16 +61,13 @@ const StyledFooter = styled.footer`
   z-index: 1000;
 `;
 
-const StyledLink = styled.a`
+const StyledSvg = styled.svg`
   fill: ${(props) =>
-    props.isActive
+    props.isActivated
       ? "var(--text-color-dark-heading)"
       : "var(--text-color-light)"};
-`;
-
-const StyledSvg = styled.svg`
   background-color: ${(props) =>
-    props.isActive ? "var(--highlight-color)" : "inherit"};
+    props.isActivated ? "var(--highlight-color)" : "inherit"};
   padding: var(--padding-x-small);
   border-radius: 50%;
 `;
