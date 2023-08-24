79434fcd2e7e2eb7a76318d3794e33480ffe9414
Fabian Rüttgers <137037086+FabianRuettgers@users.noreply.github.com>
2023-08-24 09:14:14 +0200

    Update components/MovieDetailPage/MovieDetails/ButtonSection/index.js
    Co-authored-by: Merle Makoben <merlemakoben@gmail.com>

--- components/MovieDetailPage/MovieDetails/ButtonSection/index.js
+++ components/MovieDetailPage/MovieDetails/ButtonSection/index.js
@@ -19,14 +19,14 @@ export default function ButtonSection({
     <SectionContainer>
       <BookmarkSection>
         {israted ? (
-          <SytledSvg
+          <StyledSvg
             xmlns="http://www.w3.org/2000/svg"
             height="48"
             viewBox="0 -960 960 960"
             width="48"
           >
             <path d="M378-246 154-470l43-43 181 181 384-384 43 43-427 427Z" />
-          </SytledSvg>
+          </StyledSvg>
         ) : (
           <BookmarkButton
             isBookmarked={isBookmarked}
